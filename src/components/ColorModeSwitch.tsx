5d2c096b124e184b8bcb931a43c3e9c62804c05e
sanketp25 <sanketpurohit25@gmail.com>
2023-07-28 17:32:59 -0500

    Built Search Box

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -7,7 +7,7 @@ const ColorModeSwitch = () => {
    <HStack>
         <Switch colorScheme='green' isChecked = {colorMode === 'dark'} onChange={ toggleColorMode }/>
         {/* <Text>Dark Mode</Text> */}
-        {colorMode === 'dark' ? <Text>Light Mode</Text>   : <Text>Dark Mode</Text>}
+        {colorMode === 'dark' ? <Text whiteSpace='nowrap'>Light Mode</Text>   : <Text>Dark Mode</Text>}
 
    </HStack>
   )
